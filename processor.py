77e575244f4b743c69d87df6e80fca929a28a28c
tmapana <tlotlisomapana@gmail.com>
2022-01-19 12:57:27 +0200

    Initial data processing partially completed: range-Doppler processing and range-time intensity. Next steps: motion compensation and basic SAR processing.

--- processor.py
+++ processor.py
@@ -17,27 +17,33 @@ from numpy.lib.function_base import add_newdoc_ufunc
 import matplotlib.pyplot as plt
 from numpy.linalg import matrix_rank
 
-#TODO: Range-Doppler processing
+#TODO: Motion compensation
 #TODO: Basic SAR processor
-
 
 def main():
   '''
   This is the entry point of the processor
   The main function calls these functions:
-    1. 
+    1. load_configuration_parameters
+    2. load_data
+    3. range_doppler
+    4. rti
   '''
 
   # guide user for correct input
   if (len(sys.argv) < 2):
     print("Usage: processor.py dataset_root_directory [OPTIONS]")
-    exit(-1)
+    exit(-1)  # exit on incorrect input
 
   # the first command-line argument is the root directory of the miloSAR data
   root_directory = sys.argv[1]
   
-  ''' Open file and read in the data into an array'''
+  # open file and read in the data into an array
   if os.path.exists(root_directory):
+    # create output directory for processed data
+    if not os.path.exists(os.path.join(root_directory, 'quicklook')):
+      os.mkdir(os.path.join(root_directory, 'quicklook'))
+
     # Extract configuration parameters from setup.ini and summary.ini files
     time_stamp, switch_mode, n_seconds, presummed_prf, sampling_rate, range_bins, max_range \
        = load_configuration_parameters(root_directory)
@@ -46,20 +52,42 @@ def main():
     data = load_data(root_directory, time_stamp, switch_mode, n_seconds, range_bins, presummed_prf)
 
     # OPTIONS
-    # TODO more options for future work
     if len(sys.argv) > 2:
       for opt in range(2, len(sys.argv)):
         if sys.argv[opt] == 'raw':
           # save .raw file
           save_raw(data, root_directory, time_stamp, switch_mode)
+        
+        elif sys.argv[opt] == 'rd':
+          # produce a range-Doppler plot and save to directory
+          range_doppler(data, root_directory, time_stamp, switch_mode, \
+                        n_seconds, range_bins, presummed_prf, max_range)
 
         elif sys.argv[opt] == 'rti':
           # produce range-time plot and save to diectory
-          plot_rti(data, root_directory, time_stamp, switch_mode, \
-                      n_seconds, range_bins, presummed_prf, max_range)
+          rti(data, root_directory, time_stamp, switch_mode, \
+              n_seconds, range_bins, presummed_prf, max_range)
     
         elif sys.argv[opt] == 'hist':
+          # produce a histogram of the raw data
           plot_histogram(data, root_directory=root_directory)
+
+        else:
+          print("\nUnidentified function selected.")
+          print("Usage: processor.py dataset_root_directory [OPTIONS]")
+          print("  OPTIONS:")
+          print("    raw:    Save .raw file for observing data more accurately")
+          print("    rd:     Plot range-Doppler map for each polarization")
+          print("    rti:    Plot and save range time intensity plot for each polarization")
+          print("    hist:   Plot histogram of data\n")
+
+    else:
+      print("\nUsage: processor.py dataset_root_directory [OPTIONS]")
+      print("  OPTIONS:")
+      print("    raw:    Save .raw file for observing data more accurately")
+      print("    rd:     Plot range-Doppler map for each polarization")
+      print("    rti:    Plot and save range time intensity plot for each polarization")
+      print("    hist:   Plot histogram of data\n")
               
   else:
     print("Directory", root_directory, "does not exist!")
@@ -86,7 +114,7 @@ def load_configuration_parameters(root_directory):
   n_seconds = int(summary['dataset']['n_seconds'])
   prf = int(summary['dataset']['prf'])
   sampling_rate = float(summary['dataset']['sampling_rate'])
-  presumming_factor = int(summary['integration']['n_pris'])
+  presumming_factor = int(summary['integration']['n_pris'])   # TODO: define clearly what this is
   start_index = int(summary['integration']['start_index'])
   end_index = int(summary['integration']['end_index'])
   
@@ -98,7 +126,7 @@ def load_configuration_parameters(root_directory):
   # realisable prf of data file stored in memory as data rate is halved when writing to memory
   presummed_prf = prf//presumming_factor # TODO rename to more understandable term
 
-  # range bins recorded for each pri
+  # number of range bins recorded for each pri
   range_bins = end_index - start_index + 1
   
   print("Configuration parameters loaded.")
@@ -121,11 +149,11 @@ def load_data(root_directory, time_stamp, switch_mode, n_seconds, presummed_prf,
   Each sample contains I and Q data point from two channels, A and B
   Need to extract each channel data respectively and reshape data into a 2D matrix
   '''
-  # each samples contains [Q_a, I_a, Q_b, I_b]
+  # each data sample contains [Q_a, I_a, Q_b, I_b]
   channel_a_data = data_array[1::4] + 1j*data_array[0::4]
   channel_b_data = data_array[3::4] + 1j*data_array[2::4]
 
-  # calculate the number of samples per channel
+  # calculate the number of samples recorded per channel
   n_samples_per_channel = n_seconds*presummed_prf*sample_range
 
   '''
@@ -147,6 +175,7 @@ def load_data(root_directory, time_stamp, switch_mode, n_seconds, presummed_prf,
   # Pack data into a matrix
   data = []
   if (switch_mode == 1) or (switch_mode == 2):
+    # Only two polarizations recorded for either switch mode 1 or 2
     data = np.dstack((channel_a_data, channel_b_data))
 
   elif (switch_mode == 3):
@@ -162,7 +191,9 @@ def load_data(root_directory, time_stamp, switch_mode, n_seconds, presummed_prf,
 
 def save_raw(data, root_directory, time_stamp, switch_mode):
   '''
-  Save .raw files for each I and Q channel for '''
+  Not currently saving raw data as intended
+  TODO: Rewrite this function to produce correct results
+  '''
   if (switch_mode == 1):
     # only two polarizations recorded, other channels terminated
     channel_1a = data[:,:,0]
@@ -215,15 +246,130 @@ def save_raw(data, root_directory, time_stamp, switch_mode):
       (channel_2b.imag.astype(np.int16)).tofile(raw_2b_q)
 
 
-def plot_rti(data, root_directory, time_stamp, switch_mode, n_seconds, sample_range, presummed_prf, max_range):
+def range_doppler(data, root_directory, time_stamp, switch_mode, n_seconds, sample_range, presummed_prf, max_range):
   '''
-  Produces and saves a range time intensity plot of the data
-  TODO Reduce dynamic range of image in order to see more low intensity clutter returns
+  Produces and saves a Range_Doppler plot of SAR data
+  Convert fast-time (axis=0) to range and slow-time (axis=1) to Doppler by way
+  of the Fast Fourier Transform
   '''
 
+  #TODO: compute the appropriate axes to display range against Doppler frequency
+  # x_axis = np.linspace(0, n_seconds, (int)(n_seconds*presummed_prf) , endpoint=False)  # Doppler axis
+  # y_axis = np.linspace(0, max_range, sample_range, endpoint=False)  # range axis
+
+  # select which channel to plot depending on switch mode
+  if switch_mode == 1:
+    # unpack data into the relevent polarizations depending on hardware configuration
+    channel_1a = data[:,:,0]
+    channel_1b = data[:,:,1]
+
+    # FFT in the fast-time axis
+    channel_1a_fft = np.fft.fft(channel_1a, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_1a_rd = np.fft.fft(np.fft.fft(channel_1a_fft, n=1024, axis=1))
+
+    # FFT in the fast-time axis
+    channel_1b_fft = np.fft.fft(channel_1b, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_1b_rd = np.fft.fft(np.fft.fft(channel_1b_fft, n=1024, axis=1))
+
+    # produce the range-Doppler plots
+    plot_rd_map(title='rd', data=channel_1a_rd, switch_mode=switch_mode, \
+                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    
+    plot_rd_map(title='rd', data=channel_1b_rd, switch_mode=switch_mode, \
+                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+  
+  elif switch_mode == 2:
+    # unpack data into the relevent polarizations depending on hardware configuration
+    channel_2a = data[:,:,0]
+    channel_2b = data[:,:,1]
+
+    # FFT in the fast-time axis
+    channel_2a_fft = np.fft.fft(channel_2a, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_2a_rd = np.fft.fft(np.fft.fft(channel_2a_fft, n=1024, axis=1))
+
+    # FFT in the fast-time axis
+    channel_2b_fft = np.fft.fft(channel_2b, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_2b_rd = np.fft.fft(np.fft.fft(channel_2b_fft, n=1024, axis=1))
+
+    # produce the range-Doppler plots
+    plot_rd_map(title='rd', data=channel_2a_rd, switch_mode=switch_mode, \
+                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    
+    plot_rd_map(title='rd', data=channel_2b_rd, switch_mode=switch_mode, \
+                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+
+  elif switch_mode == 3:
+    channel_1a = data[:,:,0]
+    channel_1b = data[:,:,1]
+    channel_2a = data[:,:,2]
+    channel_2b = data[:,:,3]
+
+    # FFT in the fast-time axis
+    channel_1a_fft = np.fft.fft(channel_1a, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_1a_rd = np.fft.fft(np.fft.fft(channel_1a_fft, n=1024, axis=1))
+
+    # FFT in the fast-time axis
+    channel_1b_fft = np.fft.fft(channel_1b, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_1b_rd = np.fft.fft(np.fft.fft(channel_1b_fft, n=1024, axis=1))
+
+    # FFT in the fast-time axis
+    channel_2a_fft = np.fft.fft(channel_2a, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_2a_rd = np.fft.fft(np.fft.fft(channel_2a_fft, n=1024, axis=1))
+
+    # FFT in the fast-time axis
+    channel_2b_fft = np.fft.fft(channel_2b, n=2048, axis=0)
+    # FFt in the slow-time axis
+    channel_2b_rd = np.fft.fft(np.fft.fft(channel_2b_fft, n=1024, axis=1))
+
+    # produce the range-Doppler plots for each polarization
+    plot_rd_map(title='rd', data=channel_1a_rd, switch_mode=1, \
+                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    
+    plot_rd_map(title='rd', data=channel_1b_rd, switch_mode=1, \
+                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+          
+    plot_rd_map(title='rd', data=channel_2a_rd, switch_mode=2, \
+                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    
+    plot_rd_map(title='rd', data=channel_2b_rd, switch_mode=2, \
+                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+
+
+def plot_rd_map(title, data, switch_mode, channel, root_directory, time_stamp):
+  '''
+  Plot Range-Doppler map figure to file
+  '''
+
+  plt.subplots(nrows=1,ncols=1)
+  plt.title(title.upper() + " " + str(switch_mode) + channel)
+  plt.xlabel("Doppler Bins")
+  plt.ylabel("Range Bins")
+    
+  plt.imshow(20*np.log10(np.abs(data)), aspect='auto', origin='lower')
+    
+  file_name = time_stamp + "_" + title + "_" + str(switch_mode) + channel + ".png"
+  plt.savefig(os.path.join(root_directory, 'quicklook/'+file_name))
+  
+  print(time_stamp + " " + title.upper() + " " + str(switch_mode) + channel + " saved.")
+
+
+def rti(data, root_directory, time_stamp, switch_mode, n_seconds, sample_range, presummed_prf, max_range):
+  '''
+  Produces and saves a range time intensity plot of the data
+  Compute a 2D Fast Fourier Transform to convert fast-time axis to range and slow-time axis to time
+  '''
+  #TODO: Reduce dynamic range of image in order to see more low intensity clutter returns
+
   # Compute the axes
-  x_axis = np.linspace(0, n_seconds, (int)(n_seconds*presummed_prf) , endpoint=False)  # Time axis
-  y_axis = np.linspace(0, max_range, sample_range, endpoint=False)
+  x_axis = np.linspace(0, n_seconds, (int)(n_seconds*presummed_prf) , endpoint=False)  # time axis
+  y_axis = np.linspace(0, max_range, sample_range, endpoint=False)  # range axis
 
   # select which channel to plot depending on switch mode
   if switch_mode == 1:
@@ -232,14 +378,11 @@ def plot_rti(data, root_directory, time_stamp, switch_mode, n_seconds, sample_ra
     channel_1a_fft = np.fft.fftshift(np.fft.fft2(channel_1a))
     channel_1b_fft = np.fft.fftshift(np.fft.fft2(channel_1b))
 
-    # check data
-    # plot_histogram(data=channel_1a_fft, root_directory=root_directory)
+    plot_rti(title='rti', data=channel_1a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
+              channel='a', root_directory=root_directory, time_stamp=time_stamp)
     
-    plot_figure(data=channel_1a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
-                channel='a', root_directory=root_directory, time_stamp=time_stamp)
-    
-    plot_figure(data=channel_1b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
-                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_1b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
+              channel='b', root_directory=root_directory, time_stamp=time_stamp)
   
   elif switch_mode == 2:
     channel_2a = data[:,:,0]
@@ -247,63 +390,63 @@ def plot_rti(data, root_directory, time_stamp, switch_mode, n_seconds, sample_ra
     channel_2a_fft = np.fft.fftshift(np.fft.fft2(channel_2a))
     channel_2b_fft = np.fft.fftshift(np.fft.fft2(channel_2b))
 
-    plot_figure(data=channel_2a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
-                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_2a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
+              channel='a', root_directory=root_directory, time_stamp=time_stamp)
     
-    plot_figure(data=channel_2b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
-                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_2b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=switch_mode, \
+              channel='b', root_directory=root_directory, time_stamp=time_stamp)
 
   elif switch_mode == 3:
     channel_1a = data[:,:,0]
     channel_1b = data[:,:,1]
     channel_2a = data[:,:,2]
     channel_2b = data[:,:,3]
+    
     channel_1a_fft = np.fft.fftshift(np.fft.fft2(channel_1a))
     channel_1b_fft = np.fft.fftshift(np.fft.fft2(channel_1b))
     channel_2a_fft = np.fft.fftshift(np.fft.fft2(channel_2a))
     channel_2b_fft = np.fft.fftshift(np.fft.fft2(channel_2b))
 
-    plot_figure(data=channel_1a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=1, \
-                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_1a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=1, \
+              channel='a', root_directory=root_directory, time_stamp=time_stamp)
     
-    plot_figure(data=channel_1b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=1, \
-                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_1b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=1, \
+              channel='b', root_directory=root_directory, time_stamp=time_stamp)
           
-    plot_figure(data=channel_2a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=2, \
-                channel='a', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_2a_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=2, \
+              channel='a', root_directory=root_directory, time_stamp=time_stamp)
     
-    plot_figure(data=channel_2b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=2, \
-                channel='b', root_directory=root_directory, time_stamp=time_stamp)
+    plot_rti(title='rti', data=channel_2b_fft, x_axis=x_axis, y_axis=y_axis, switch_mode=2, \
+              channel='b', root_directory=root_directory, time_stamp=time_stamp)
 
 
-def plot_figure(data, x_axis, y_axis, switch_mode, channel, root_directory, time_stamp):
+def plot_rti(title, data, x_axis, y_axis, switch_mode, channel, root_directory, time_stamp):
   '''
-  Plot and save figure to file
+  Plot and save Range-Time Intensity to file
   '''
 
   plt.subplots(nrows=1,ncols=1)
-  plt.title("Range Time Intensity")
+  plt.title(title.upper() + " " + str(switch_mode) + channel)
   plt.xlabel("Slow Time [s]")
   plt.ylabel("Range [m]")
   
-  avg = np.mean(data) # average in fast time axis
-  data_min = np.amin(data)
-  data_max = np.amax(data)
-  data = np.clip(data, data_min, data_max)
-  #data = (data-data_min)/(data_max - data_min)
+  data_log10 = 20*np.log10(np.abs(data)) - np.amax(20*np.log10(np.abs(data)))
   
-  plt.imshow(20*np.log10(np.abs(data)), aspect='auto', origin='lower', extent=[min(x_axis), max(x_axis), min(y_axis), max(y_axis)])
+  plt.imshow(data_log10, aspect='auto', origin='lower', extent=[min(x_axis), max(x_axis), min(y_axis), max(y_axis)])
+    
+  file_name = time_stamp + "_" + title + "_" + str(switch_mode) + channel + ".png"
+  plt.savefig(os.path.join(root_directory, 'quicklook/'+file_name))
   
-  file_name = time_stamp + "_" + str(switch_mode) + channel + "_rti.png"
-  plt.savefig(os.path.join(root_directory, file_name))
-  
-  print("Range-time plot saved.")
+  print(time_stamp + " " + title.upper() + " " + str(switch_mode) + channel + " saved.")
 
 
 def plot_histogram(data, root_directory):
   '''
   Plot a histogram of the data to visualise the spread of data
+  Not currently working as intended
   '''
+  #TODO: Determine what information to plot that will make a meaning report of the data
+
   channel_1a = data[:,:,0]
   channel_1b = data[:,:,1]
   channel_1a_fft = np.fft.fftshift(np.fft.fft(channel_1a, axis=0))
@@ -321,4 +464,4 @@ def plot_histogram(data, root_directory):
 
 
 if __name__ == "__main__":
-  main()
+  main()  # call the main function
